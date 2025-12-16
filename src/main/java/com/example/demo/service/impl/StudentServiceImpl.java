78acb85a64c4738aaf47ec97d57d12976186f87e
bala-amypo <bala@amypo.in>
2025-12-16 16:28:30 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,25 +1,41 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.Student;
-import com.example.demo.exception.DuplicateStudentException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentServiceImpl implements StudentService {
 
-    private final StudentRepository studentRepository;
+    private final StudentRepository repository;
 
-    public StudentServiceImpl(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+    public StudentServiceImpl(StudentRepository repository) {
+        this.repository = repository;
     }
 
     @Override
     public Student createStudent(Student student) {
-        studentRepository.findByRollNumber(student.getRollNumber())
-                .ifPresent(s -> { throw new DuplicateStudentException(); });
+        return repository.save(student);
+    }
 
-        return studentRepository.save(student);
+    @Override
+    public Student getStudentById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found with id " + id));
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        Student student = getStudentById(id);
+        repository.delete(student);
     }
 }
