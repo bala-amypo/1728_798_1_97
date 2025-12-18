f5dea309657b1ae4d767e52d5bd3c321e3a2b334
bala-amypo <bala@amypo.in>
2025-12-18 05:17:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationLog.java
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -9,5 +9,20 @@ import java.time.LocalDateTime;
 @Builder
 public class VerificationLog{
 
-    @
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private Certificate certificate;
+
+    @Column(nullable = false)
+    private LocalDateTime verifiedAt;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String ipAddress;
 }
