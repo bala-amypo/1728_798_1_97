c9f92e51eb210aa5764cdd27e018ec2607b7cd3a
bala-amypo <bala@amypo.in>
2025-12-18 09:29:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationLog.java
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 public class VerificationLog{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
