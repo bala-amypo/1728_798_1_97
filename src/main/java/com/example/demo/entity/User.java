94f02b92489ac5f3bc249ce26854b6548ce00db0
bala-amypo <bala@amypo.in>
2025-12-18 04:42:38 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,17 @@
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsContstructor
+@AllArgsConstructor
+@Builder
+public class User{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    
+}
