efe148c294d72096035a48d8e74fe133440e85d7
bala-amypo <bala@amypo.in>
2025-12-18 05:12:38 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -0,0 +1,13 @@
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VerificationLog{
+
+    @
+}
