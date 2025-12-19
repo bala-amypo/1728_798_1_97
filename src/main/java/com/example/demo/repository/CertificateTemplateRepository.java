58f31bb5986de1b40defa1b46e11bf1550764583
bala-amypo <bala@amypo.in>
2025-12-19 08:08:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
+++ src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
@@ -2,10 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.CertificateTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 public interface CertificateTemplateRepository extends JpaRepository<CertificateTemplate, Long> {
-    boolean existsByTemplateName(String templateName);  // Check if a template with this name exists
-//     Optional<CertificateTemplate> findByTemplateName(String templateName);  // Retrieve template by name
+    Optional<CertificateTemplate> findByTemplateName(String templateName);
 }
