49de6f91bae47add31e8d56b5761873cabdddf36
bala-amypo <bala@amypo.in>
2025-12-27 09:13:11 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -14,12 +17,19 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("https://9264.pro604cr.amypo.ai/");
+
+        List<Server> servers = Collections.singletonList(server);
+
         return new OpenAPI()
-                .servers(List.of(
-                        new Server().url("https://9264.pro604cr.amypo.ai/")))
-                .info(new Info().title("Digital Certificate Generator API").version("1.0"))
+                .servers(servers)
+                .info(new Info()
+                        .title("Digital Certificate Generator API")
+                        .version("1.0"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
+                .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
                                         .type(SecurityScheme.Type.HTTP)
